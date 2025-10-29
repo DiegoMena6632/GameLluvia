5e28abdf7bd647b49904692cc8941fa43e4f9d14
GorillaSetups <GorillaSetups@DESKTOP-V2AD5CQ.lan>
2025-10-29 20:09:18 -0300

    commit bueno

--- core/src/main/java/ayud/ejemplo/GotaMala.java
+++ core/src/main/java/ayud/ejemplo/GotaMala.java
@@ -1,5 +1,19 @@
 package ayud.ejemplo;
 
-public class GotaMala {
+// GotaMala extiende la clase abstracta
+public class GotaMala extends Droppable
+{
+	
+    public GotaMala(float x, float y, float velocidadY)
+    {
+        // Llama al constructor padre con la ruta de su textura
+        super(x, y,"dropBad.png", velocidadY);
+    }
 
+    // Implementación del método abstracto:
+    @Override
+    public void aplicarEfecto(Tarro tarro) {
+        // Lógica: Dañar al tarro (usa el método que ya tienes en Tarro)
+        tarro.dañar();
+    }
 }
